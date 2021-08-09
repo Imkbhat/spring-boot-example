12dff72b6410db86225646fdccac147ce318f2c6
Kiran B S <kbs71190@gmail.com>
2021-08-09 19:32:07 +0530

    Implemented OpenApi Specification and Swagger

--- sb-practice/src/main/java/com/luv2codeinjava/practice/SbPracticeApplication.java
+++ sb-practice/src/main/java/com/luv2codeinjava/practice/SbPracticeApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title="Department API",version = "2.0", description = "Specification for Department API" ))
 public class SbPracticeApplication {
 
 	public static void main(String[] args) {
